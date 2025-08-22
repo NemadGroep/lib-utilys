6d96f40e749e2daf36e2a060881e81138c5fe196
Nathan Blum <n.blum@student.tudelft.nl>
2025-08-22 09:21:55 +0200

    add utils

--- src/lib_utilys/utils.py
+++ src/lib_utilys/utils.py
@@ -1,6 +1,8 @@
+import re
 import json
 import logging
 from pathlib import Path
+from datetime import date, datetime
 
 logger = logging.getLogger(__name__)
 
@@ -20,10 +22,27 @@ def read_json(path: Path) -> dict:
         logger.exception("Failed to read JSON file at %s", path)
         return {}
 
-def write_json(path: Path, data: dict):
-    """Writes data to a JSON file."""
-    try:
-        with open(path, 'w') as file:
-            json.dump(data, file, indent=4)
-    except Exception as e:
-        logger.exception("Error writing JSON file at %s", path)
+def write_json(path, data):
+    def _default(o):
+        if isinstance(o, (date, datetime)):
+            return o.isoformat()
+        raise TypeError(f"Type {type(o)} not serializable")
+
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4, default=_default)
+
+def extract_business(email_adress: str) -> str | None:
+    """Extracts the business name from an email address."""
+    match = re.search(r'[\w\.-]+@[\w\.-]+', email_adress)
+    extracted_email = match.group(0) if match else email_adress
+    email_parts = extracted_email.split("@")
+    if len(email_parts) == 2:
+        domain_parts = email_parts[1].split(".")
+        if len(domain_parts) > 1:
+            business_name = domain_parts[0]
+            return business_name
+    return None
+
+def clean_xml(text: str) -> str:
+    return re.sub(r'&(?!amp;|lt;|gt;|quot;|apos;|#\d+;)', '&amp;', text)
+
